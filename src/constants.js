60f915d9368a312cdcc95f3c73a1565fd28e4ee7
rybalchenko.dn <dmitrii.rybalchenko@domru.ru>
2020-11-29 18:25:28 +0500

    add logger

--- src/constants.js
+++ src/constants.js
@@ -22,3 +22,8 @@ module.exports.HttpCode = {
 module.exports.MAX_ID_LENGTH = 6;
 
 module.exports.API_PREFIX = `/api`;
+
+module.exports.Env = {
+  DEVELOPMENT: `development`,
+  PRODUCTION: `production`
+};
